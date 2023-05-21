7c134a1279e68c5ba2eac0549b86385c3f7ec4bd
PenginAction <udonfox@gmail.com>
2023-05-21 20:57:09 +0900

    bittflyerではなく、bittradeに変更

--- appconfig/appconfig.go
+++ appconfig/appconfig.go
@@ -12,8 +12,8 @@ type Configuration struct {
 	GmoapiSecret  string
 	Symbol1       string
 	Symbol2       string
-	BitFapiKey    string
-	BitFapiSecret string
+	BitapiKey    string
+	BitapiSecret string
 	Symbol3       string
 	LogFile       string
 	Dbuser        string
@@ -39,8 +39,8 @@ func init() {
 		Symbol1:       cfg.Section("GMOCoin").Key("gmosymbol1").String(),
 		Symbol2:       cfg.Section("GMOCoin").Key("gmosymbol2").String(),
 		Symbol3:       cfg.Section("GMOCoin").Key("gmosymbol3").String(),
-		BitFapiKey:    cfg.Section("bitflyer").Key("bitfapi_key").String(),
-		BitFapiSecret: cfg.Section("bitflyer").Key("bitfapi_secret").String(),
+		BitapiKey:    cfg.Section("bittrade").Key("bitapi_key").String(),
+		BitapiSecret: cfg.Section("bittrade").Key("bitapi_secret").String(),
 		LogFile:       cfg.Section("assetmanagement").Key("log_file").String(),
 		Dbuser:        cfg.Section("database").Key("dbuser").String(),
 		Dbpass:        cfg.Section("database").Key("dbpass").String(),
