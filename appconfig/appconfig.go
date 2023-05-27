ca3e83df2930de4b7d19a77ea2bfe60d4e7b767f
PenginAction <udonfox@gmail.com>
2023-05-27 22:38:54 +0900

    フォーマットを整頓

--- appconfig/appconfig.go
+++ appconfig/appconfig.go
@@ -8,19 +8,19 @@ import (
 )
 
 type Configuration struct {
-	GmoapiKey    string
-	GmoapiSecret string
-	GmoCoinSymbols []string
-	BitapiKey    string
-	BitapiSecret string
+	GmoapiKey       string
+	GmoapiSecret    string
+	GmoCoinSymbols  []string
+	BitapiKey       string
+	BitapiSecret    string
 	BittradeSymbols []string
-	LogFile      string
-	Dbuser       string
-	Dbpass       string
-	Dblocalhost  string
-	Dbport       int
-	Dbname       string
-	Port         int
+	LogFile         string
+	Dbuser          string
+	Dbpass          string
+	Dblocalhost     string
+	Dbport          int
+	Dbname          string
+	Port            int
 }
 
 var AppConfig Configuration
@@ -33,18 +33,18 @@ func init() {
 	}
 
 	AppConfig = Configuration{
-		GmoapiKey:    cfg.Section("GMOCoin").Key("gmoapi_key").String(),
-		GmoapiSecret: cfg.Section("GMOCoin").Key("gmoapi_secret").String(),
-		GmoCoinSymbols: cfg.Section("GMOCoin").Key("Symbols").Strings(","),
-		BitapiKey:    cfg.Section("bittrade").Key("bitapi_key").String(),
-		BitapiSecret: cfg.Section("bittrade").Key("bitapi_secret").String(),
+		GmoapiKey:       cfg.Section("GMOCoin").Key("gmoapi_key").String(),
+		GmoapiSecret:    cfg.Section("GMOCoin").Key("gmoapi_secret").String(),
+		GmoCoinSymbols:  cfg.Section("GMOCoin").Key("Symbols").Strings(","),
+		BitapiKey:       cfg.Section("bittrade").Key("bitapi_key").String(),
+		BitapiSecret:    cfg.Section("bittrade").Key("bitapi_secret").String(),
 		BittradeSymbols: cfg.Section("bittrade").Key("Symbols").Strings(","),
-		LogFile:      cfg.Section("assetmanagement").Key("log_file").String(),
-		Dbuser:       cfg.Section("database").Key("dbuser").String(),
-		Dbpass:       cfg.Section("database").Key("dbpass").String(),
-		Dblocalhost:  cfg.Section("database").Key("dblocalhost").String(),
-		Dbport:       cfg.Section("database").Key("dbport").MustInt(),
-		Dbname:       cfg.Section("database").Key("dbname").String(),
-		Port:         cfg.Section("web").Key("port").MustInt(),
+		LogFile:         cfg.Section("assetmanagement").Key("log_file").String(),
+		Dbuser:          cfg.Section("database").Key("dbuser").String(),
+		Dbpass:          cfg.Section("database").Key("dbpass").String(),
+		Dblocalhost:     cfg.Section("database").Key("dblocalhost").String(),
+		Dbport:          cfg.Section("database").Key("dbport").MustInt(),
+		Dbname:          cfg.Section("database").Key("dbname").String(),
+		Port:            cfg.Section("web").Key("port").MustInt(),
 	}
 }
