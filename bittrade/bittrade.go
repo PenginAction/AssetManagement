bfefd866e870007159938fbc2030b658e26cd452
PenginAction <udonfox@gmail.com>
2023-05-27 12:33:18 +0900

    ユーザーアカウントの情報を取得．

--- bittrade/bittrade.go
+++ bittrade/bittrade.go
@@ -1,14 +1,12 @@
 package bittrade
 
 import (
-	"bytes"
 	"crypto/hmac"
 	"crypto/sha256"
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"net/url"
 	"sort"
@@ -16,190 +14,97 @@ import (
 	"time"
 )
 
-const baseURL = "https://api-cloud.bittrade.co.jp"
 
-type BitTradeAPIClient struct {
-	key        string
-	secret     string
-	httpClient *http.Client
+const API_URL = "https://api-cloud.bittrade.co.jp"
+
+type Client struct {
+	AccessKey string  
+	SecretKey string  
 }
 
-func NewBitTradeAPIClient(key, secret string) *BitTradeAPIClient{
-	apiClient := &BitTradeAPIClient{key, secret, &http.Client{}}
-	log.Println("New BitTradeAPIClient created.")
-	return apiClient
+func NewClient(accessKey, secretKey string) *Client {
+	return &Client{
+		AccessKey: accessKey,
+		SecretKey: secretKey,
+	}
 }
 
-func (api BitTradeAPIClient) header(method, endpoint string, params map[string]string) map[string]string {
-	// パラメータをソート．
+// signメソッドはペイロードを署名
+func (c *Client) sign(payload string) string {
+	h := hmac.New(sha256.New, []byte(c.SecretKey))
+	h.Write([]byte(payload))
+	return base64.StdEncoding.EncodeToString(h.Sum(nil))
+}
+
+// sendRequestメソッドは指定したパスにリクエストを送信
+func (c *Client) sendRequest(path string) (string, error) {
+	params := map[string]string{
+		"AccessKeyId":      c.AccessKey,
+		"SignatureMethod":  "HmacSHA256",
+		"SignatureVersion": "2",
+		"Timestamp":        time.Now().UTC().Format("2006-01-02T15:04:05"),
+	}
+
+	// キーを昇順にソート
 	var keys []string
 	for k := range params {
 		keys = append(keys, k)
 	}
 	sort.Strings(keys)
-	log.Println("Parameters sorted.")
 
-	// クエリ文字列を作成．
-	query := ""
+	// ソートされたクエリ文字列を作成
+	var queryString string
 	for _, k := range keys {
-		query += fmt.Sprintf("%s=%s&", url.QueryEscape(k), url.QueryEscape(params[k]))
+		queryString += fmt.Sprintf("%s=%s&", k, url.QueryEscape(params[k]))
 	}
-	// 末尾の'&'を削除
-	query = strings.TrimSuffix(query, "&")
-	log.Println("Query string created: ", query)
+	queryString = strings.TrimSuffix(queryString, "&")
 
-	// 署名する文字列を作成する．
-	timestamp := time.Now().UTC().Format("2006-01-02T15:04:05")
-	stringToSign := fmt.Sprintf("%s\n%s\n%s\n%s", method, endpoint, query, timestamp)
+	// 署名のペイロードを作成
+	payload := fmt.Sprintf("GET\napi-cloud.bittrade.co.jp\n%s\n%s", path, queryString)
 
-	log.Println("String to sign created: ", stringToSign)
+	// 署名を作成します。
+	signature := c.sign(payload)
 
-	//文字列の署名
-	sign := api.sign(stringToSign)
-	log.Println("Signature created: ", sign)
+	// 署名をクエリ文字列に追加
+	queryString += fmt.Sprintf("&Signature=%s", url.QueryEscape(signature))
 
-	headers := map[string]string{
-		"Content-Type":     "application/json",
-		"ACCESS-KEY":       api.key,
-		"SIGNATURE-METHOD": "HmacSHA256",
-		"SIGNATURE-VERSION": "2",
-		"TIMESTAMP":        timestamp,
-		"SIGNATURE":        sign,
+	// リクエストを送信
+	resp, err := http.Get(fmt.Sprintf("%s%s?%s", API_URL, path, queryString))
+	if err != nil {
+		return "", err
 	}
-	log.Println("Headers created: ", headers)
-	return headers
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(body), nil
 }
 
 
-func (api *BitTradeAPIClient) sign(message string) string {
-	h := hmac.New(sha256.New, []byte(api.secret))
-	h.Write([]byte(message))
-	signature := base64.StdEncoding.EncodeToString(h.Sum(nil))
-	log.Println("Message signed: ", signature)
-	return url.QueryEscape(signature)
+type Account struct {
+	Status string `json:"status"`
+	Data   []struct {
+		ID     int    `json:"id"`
+		Type   string `json:"type"`
+		Subtype string `json:"subtype"`
+		State   string `json:"state"`
+	} `json:"data"`
 }
 
-func (api *BitTradeAPIClient) Request(method, endpoint string, params map[string]string) (string, error){
-	headers := api.header(method, endpoint, params)
-
-	reqBody := bytes.NewBufferString("")
-	if method == "POST"{
-		jsonValue, _ := json.Marshal(params)
-		reqBody = bytes.NewBuffer(jsonValue)
-	}
-
-	req, err := http.NewRequest(method, baseURL+endpoint, reqBody)
-	if err != nil{
-		return "", fmt.Errorf("failed to create HTTP request: %v", err)
-	}
-
-	for k, v := range headers{
-		req.Header.Set(k, v)
-	}
-
-	log.Println("Request headers: ", req.Header)
-
-	res, err := api.httpClient.Do(req)
-	if err != nil{
-		return "", fmt.Errorf("failed to send HTTP request: %v", err)
-	}
-	defer res.Body.Close()
-
-	log.Printf("Response status: %s", res.Status)
-	bodyBytes, _ := ioutil.ReadAll(res.Body)
-	bodyString := string(bodyBytes)
-	log.Printf("Response body: %s", bodyString)
-
-	var buf bytes.Buffer
-	err = json.Indent(&buf, bodyBytes, "", " ")
-	if err != nil{
-		return "", fmt.Errorf("failed to indent JSON response: %v", err)
-	}
-
-	return buf.String(), nil
-}
-
-
-type UserInfoData struct{
-	ID     int     `json:"id"`
-	Type   string  `json:"type"`
-	State  string  `json:"state"`
-	UserID int     `json:"user-id"`
-}
-
-type UserInfoResponse struct{
-	Status string     `json:"status"`
-	Data   UserInfoData `json:"data"`
-}
-
-func (api *BitTradeAPIClient) GetAccountInfo() (*UserInfoResponse, error){
-	method := "GET"
-	path := "/v1/account/accounts"
-	params := map[string]string{}
-
-	resp, err := api.Request(method, path, params)
-	if err != nil{
+func (c *Client) GetUserAccount() (*Account, error) {
+	resp, err := c.sendRequest("/v1/account/accounts")
+	if err != nil {
 		return nil, err
 	}
 
-	var userinfoResponse UserInfoResponse
-	err = json.Unmarshal([]byte(resp), &userinfoResponse)
-	if err != nil{
+	var account Account
+	err = json.Unmarshal([]byte(resp), &account)
+	if err != nil {
 		return nil, err
 	}
 
-	if userinfoResponse.Status != "ok"{
-		return nil, fmt.Errorf("API returned an error status: %s", userinfoResponse.Status)
-	}
-	
-	return &userinfoResponse, nil
-}
-
-
-type Asset struct {
-	Currency string `json:"currency"`
-	Type     string `json:"type"`
-	Balance  string `json:"balance"`
-}
-
-type AssetsData struct {
-	ID     int     `json:"id"`
-	Type   string  `json:"type"`
-	State  string  `json:"state"`
-	List   []Asset `json:"list"`
-	UserID int     `json:"user-id"`
-}
-
-type AssetsResponse struct {
-	Status string     `json:"status"`
-	Data   AssetsData `json:"data"`
-}
-
-func (api *BitTradeAPIClient) GetAssets() (*AssetsResponse, error){
-	userInfo, err := api.GetAccountInfo()
-	if err != nil{
-		return nil, err
-	}
-
-	accountID := userInfo.Data.ID
-	path := fmt.Sprintf("/v1/account/accounts/%d/balance", accountID)
-	method := "GET"
-	params := make(map[string]string)
-
-	resp, err := api.Request(method, path, params)
-	if err != nil{
-		return nil, err
-	}
-
-	var assetsResponse AssetsResponse
-	err = json.Unmarshal([]byte(resp), &assetsResponse)
-	if err != nil{
-		return nil, err
-	}
-
-	if assetsResponse.Status != "ok"{
-		return nil, fmt.Errorf("API returned an error status: %s", assetsResponse.Status )
-	}
-	
-	return &assetsResponse, nil
+	return &account, nil
 }
